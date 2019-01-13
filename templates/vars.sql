ae3b839112aa5f02ee43e99b6c23955dabaacc0f
Andrey Chalov <andrchalov@gmail.com>
2019-01-13 13:26:49 +0300

    arrays and objects in vars support

--- templates/vars.sql
+++ templates/vars.sql
@@ -3,6 +3,7 @@
 --
 
 DROP FUNCTION IF EXISTS pgver.vars();
+DROP FUNCTION IF EXISTS pgver.var(variadic text[]);
 
 {% if pgver_vars %}
 -------------------------------------------------------------------------------
@@ -17,6 +18,36 @@ SELECT
 {% for key, value in pgver_vars.iteritems() %}
   $var${{value}}$var${% if not loop.last %},{% endif %}
 {% endfor %}
+$function$;
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+CREATE FUNCTION pgver.vars_jsonb()
+  RETURNS jsonb
+  LANGUAGE sql
+  IMMUTABLE
+AS $function$
+SELECT $var${{pgver_vars|tojson|safe}}$var$::jsonb
+$function$;
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+CREATE FUNCTION pgver.var(VARIADIC text[])
+  RETURNS text
+  LANGUAGE plpgsql
+  IMMUTABLE
+AS $function$
+DECLARE
+  v text;
+BEGIN
+  v = $var${{pgver_vars|tojson|safe}}$var$::jsonb #>> $1;
+
+  IF v ISNULL THEN
+    RAISE 'pgver.var(): Unknown path';
+  END IF;
+
+  RETURN v;
+END;
 $function$;
 -------------------------------------------------------------------------------
 {% endif %}
