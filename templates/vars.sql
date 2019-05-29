2de032e6a459d0429479e620cf0266fe83270b3f
Andrey Chalov <andrchalov@gmail.com>
2019-05-29 13:59:58 +0300

    removed pgver.var() and pgver.vars_jsonb()

--- templates/vars.sql
+++ templates/vars.sql
@@ -3,8 +3,6 @@
 --
 
 DROP FUNCTION IF EXISTS pgver.vars();
-DROP FUNCTION IF EXISTS pgver.vars_jsonb();
-DROP FUNCTION IF EXISTS pgver.var(variadic text[]);
 
 {% if pgver_vars %}
 -------------------------------------------------------------------------------
@@ -19,36 +17,6 @@ SELECT
 {% for key, value in pgver_vars.iteritems() %}
   $var${{value}}$var${% if not loop.last %},{% endif %}
 {% endfor %}
-$function$;
--------------------------------------------------------------------------------
-
--------------------------------------------------------------------------------
-CREATE FUNCTION pgver.vars_jsonb()
-  RETURNS jsonb
-  LANGUAGE sql
-  IMMUTABLE
-AS $function$
-SELECT $var${{pgver_vars|tojson|safe}}$var$::jsonb
-$function$;
--------------------------------------------------------------------------------
-
--------------------------------------------------------------------------------
-CREATE FUNCTION pgver.var(VARIADIC text[])
-  RETURNS text
-  LANGUAGE plpgsql
-  IMMUTABLE
-AS $function$
-DECLARE
-  v text;
-BEGIN
-  v = $var${{pgver_vars|tojson|safe}}$var$::jsonb #>> $1;
-
-  IF v ISNULL THEN
-    RAISE 'pgver.var(): Unknown path';
-  END IF;
-
-  RETURN v;
-END;
 $function$;
 -------------------------------------------------------------------------------
 {% endif %}
