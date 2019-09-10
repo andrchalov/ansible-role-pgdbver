f400b7ae667864ab52028b853ca990bc19abdfff
Andrey Chalov <andrchalov@gmail.com>
2019-09-10 14:54:55 +0300

    fixes for python3

--- templates/vars.sql
+++ templates/vars.sql
@@ -7,14 +7,14 @@ DROP FUNCTION IF EXISTS pgver.vars();
 {% if pgver_vars %}
 -------------------------------------------------------------------------------
 CREATE FUNCTION pgver.vars(
-{% for key, value in pgver_vars.iteritems() %}
+{% for key, value in pgver_vars.items() %}
   OUT "{{key}}" text{% if not loop.last %},{% endif %}
 {% endfor %}
 )
   LANGUAGE sql
 AS $function$
 SELECT
-{% for key, value in pgver_vars.iteritems() %}
+{% for key, value in pgver_vars.items() %}
   $var${{value}}$var${% if not loop.last %},{% endif %}
 {% endfor %}
 $function$;
